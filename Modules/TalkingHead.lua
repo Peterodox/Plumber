aa457a0350030d0b87b4fdb35b205231571d1a51
Peter Yu <49627468+Peterodox@users.noreply.github.com>
2026-05-14 19:35:58 +0800

    SetAlphaGradient Doesn't Work Well With SLUG
    - Texts become invisible.

--- Modules/TalkingHead.lua
+++ Modules/TalkingHead.lua
@@ -222,7 +222,7 @@ function NewTalkingHead:SetFontHeightByPercentage(percentage)
 
 	local style, gray;
 	if DB.TalkingHead_TextOutline then
-		style = "SLUGOUTLINE";
+		style = "OUTLINE";
 		gray = 0.898;   --VERY_LIGHT_GRAY_COLOR
 	else
 		style = "";
