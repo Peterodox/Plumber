b03d5d0f50c40f1fbeafb1d2a69757fd5e97d9ec
Peter Yu <49627468+Peterodox@users.noreply.github.com>
2026-09-03 21:40:51 +0800

    Enable Transmog Restore by default

--- Initialization.lua
+++ Initialization.lua
@@ -246,7 +246,7 @@ local DefaultValues = {
 	CatalystUI = true,					--Allow Ctrl-Click to preview items in Dressing Room for UIs that don't natively support this action
 	HuntTable = true,					--Replace generic quest icons with difficulties and add achievement indicators.
 	PreyQuestSuperTrack = true,			--During the final stage, clicking the Prey widget also super track the target location.
-	BlizzFixActionBarArt = true,		--Fix Action Bar Art Reappears After Hide/Show UI
+	TransmogRaestorePending = true,		--Restore unsaved transmog changes
 
 
 	--Tooltip
@@ -417,6 +417,7 @@ local DefaultValues = {
 	--BlizzFixWardrobeTrackingTip = true,   --Hide Wardrobe tip that cannot be disabled   --Tip removed by Blizzard
 	--MinimapMouseover = false,             --Ridden with compatibility issue
 	--LandingPageSwitch = true,             --Right click on ExpansionLandingPageMinimapButton to open a menu to access mission report  --Merged into NewExpansionLandingPage
+	--BlizzFixActionBarArt = true,			--Fix Action Bar Art Reappears After Hide/Show UI
 };
 
 
